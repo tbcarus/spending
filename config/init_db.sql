bc4318581861fc01e9763b15c5b6a5b176e3c1f9
tbcarus <70680359+tbcarus@users.noreply.github.com>
2022-04-29 21:49:26 +0300

    init_db.sql fix

--- config/init_db.sql
+++ config/init_db.sql
@@ -1,18 +1,38 @@
 create table costs
 (
-    -- Only integer types can be auto increment
-    id char(36) default nextval('costs_id_seq'::regclass) not null
-        constraint costs_pk
+    id char(36) not null
+        constraint costs_pkey
             primary key,
     type text,
-    prise integer not null,
+    price integer not null,
     description text,
-    date date default now() not null,
+    date timestamp default now() not null,
     user_id char(36)
+        constraint costs_user_id_fk
+            references users
+            on delete cascade
 );
-
-alter table costs owner to postgres;
 
 create unique index costs_id_uindex
     on costs (id);
+
+create table users
+(
+    id char(36) not null
+        constraint users_pk
+            primary key,
+    name text default 'Имя не задано'::text,
+    email text not null,
+    password text not null,
+    start_period_date timestamp default now() not null
+);
+
+create unique index users_email_uindex
+    on users (email);
+
+create unique index users_id_uindex
+    on users (id);
+
+
+
 
